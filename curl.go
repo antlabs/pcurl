83203c7b0650999fd4567672b568e0487975ff58
guonaihong <guonaihong@qq.com>
2020-03-10 22:47:59 +0800

    更新伪代码

--- curl.go
+++ curl.go
@@ -2,6 +2,7 @@ package pcurl
 
 import (
 	"github.com/guonaihong/clop"
+	"github.com/guonaihong/gout"
 	"net/http"
 )
 
@@ -9,6 +10,7 @@ type Curl struct {
 	Method string   `clop:"-X; --request" usage:"Specify request command to use"`
 	Header []string `clop:"-H; --header" usage:"Pass custom header(s) to server"`
 	Data   string   `clop:"-d; --data"   usage:"HTTP POST data"`
+	URL    string   `clop:"args=url" usage:"url"`
 
 	Err error
 }
@@ -20,6 +22,17 @@ func Parse(curl string) *Curl {
 	return &c
 }
 
+func ParseAndRequest(curl string) (*http.Request, error) {
+	return Parse(curl).Request()
+}
+
 func (c *Curl) Request() (*http.Request, error) {
+	if len(c.Method) == 0 && len(c.Data) > 0 {
+		c.Method = "POST"
+	}
+
+	// TODO open
+	//err := gout.New().Method(c.Method).URL(c.URL).SetBody(c.Data).Do()
+
 	return nil, nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/antlabs/pcurl
 
 go 1.13
 
-require github.com/guonaihong/clop v0.0.0-20200309133917-2e9fddd3a5e8
+require (
+	github.com/guonaihong/clop v0.0.0-20200309133917-2e9fddd3a5e8
+	github.com/guonaihong/gout v0.0.9
+)
