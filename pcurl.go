569bef289ec77ffc6593cb24bc52ba46509b36ea
guonaihong <guonaihong@qq.com>
2020-03-24 22:01:47 +0800

    新增测试http header代码

--- pcurl.go
+++ pcurl.go
@@ -1,6 +1,7 @@
 package pcurl
 
 import (
+	"fmt"
 	"github.com/guonaihong/clop"
 	"github.com/guonaihong/gout"
 	"net/http"
@@ -12,6 +13,7 @@ type Curl struct {
 	Method string   `clop:"-X; --request" usage:"Specify request command to use"`
 	Header []string `clop:"-H; --header" usage:"Pass custom header(s) to server"`
 	Data   string   `clop:"-d; --data"   usage:"HTTP POST data"`
+	Form   string   `clop:"-F; --form" usage:"Specify multipart MIME data"`
 	URL    string   `clop:"args=url" usage:"url"`
 
 	Err error
@@ -30,10 +32,33 @@ func ParseSlice(curl []string) *Curl {
 	if len(curl) > 0 && strings.ToLower(curl[0]) == "curl" {
 		curl = curl[1:]
 	}
+	fmt.Printf("%#v\n", curl)
 
 	p := clop.New(curl).SetExit(false)
 	c.Err = p.Bind(&c)
 	return &c
+}
+
+func (c *Curl) createHeader() []string {
+	if len(c.Header) == 0 {
+		return nil
+	}
+
+	header := make([]string, len(c.Header)*2)
+	index := 0
+	for _, v := range header {
+		hv := strings.Split(v, ":")
+		if len(hv) != 2 {
+			continue
+		}
+
+		header[index] = hv[0]
+		index++
+		header[index] = hv[1]
+		index++
+	}
+
+	return header
 }
 
 func (c *Curl) Request() (*http.Request, error) {
@@ -41,7 +66,11 @@ func (c *Curl) Request() (*http.Request, error) {
 		c.Method = "POST"
 	}
 
-	var data interface{}
+	var (
+		data interface{}
+	)
+
+	header := c.createHeader()
 
 	data = c.Data
 	if len(c.Data) > 0 && c.Data[0] == '@' {
@@ -55,5 +84,15 @@ func (c *Curl) Request() (*http.Request, error) {
 		data = fd
 	}
 
-	return gout.New().SetMethod(c.Method).Debug(true).SetURL(c.URL).SetBody(data).Request()
+	g := gout.New().
+		SetMethod(c.Method). //设置method POST or GET or DELETE
+		Debug(true)          //打开debug模式
+
+	if header != nil {
+		g.SetHeader(header) //设置http header
+	}
+
+	return g.SetURL(c.URL). //设置url
+				SetBody(data). //设置http body
+				Request()      //获取*http.Request
 }
--- pcurl_test.go
+++ pcurl_test.go
@@ -1,6 +1,7 @@
 package pcurl
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/guonaihong/gout"
 	"github.com/stretchr/testify/assert"
@@ -8,6 +9,8 @@ import (
 	"net/http/httptest"
 	"testing"
 )
+
+type H map[string]interface{}
 
 func createGeneral(data string) *httptest.Server {
 	router := func() *gin.Engine {
@@ -28,14 +31,71 @@ func createGeneral(data string) *httptest.Server {
 func Test_ParseSlice(t *testing.T) {
 
 	// 创建测试服务
-
 	need := `{"key":"val"}`
 	ts := createGeneral(need)
 	got := ""
 	s := []string{"curl", "-X", "POST", "-d", need, ts.URL}
 	req, err := ParseSlice(s).Request()
 	assert.NoError(t, err)
+
 	err = gout.New().SetRequest(req).Debug(true).BindBody(&got).Do()
 	assert.NoError(t, err)
 	assert.Equal(t, need, got)
+}
+
+func createGeneralHeader(need H, t *testing.T) *httptest.Server {
+	router := func() *gin.Engine {
+		router := gin.New()
+
+		router.POST("/", func(c *gin.Context) {
+			var gotHeader H
+			c.BindHeader(&gotHeader)
+			fmt.Printf("==================gotHeader:%s\n", gotHeader)
+			if assert.Equal(t, need, gotHeader) {
+				c.String(200, "")
+				return
+			}
+
+			c.String(500, "")
+		})
+
+		return router
+	}()
+
+	return httptest.NewServer(http.HandlerFunc(router.ServeHTTP))
+}
+
+func Test_Header(t *testing.T) {
+
+	type testHeader struct {
+		curlHeader []string
+		need       H
+	}
+
+	for _, headerData := range []testHeader{
+		testHeader{
+			curlHeader: []string{"curl", "-X", "POST", "-H", "h1:v1", "-H", "h2:v2"},
+			need: H{
+				"h1": "v1",
+				"h2": "v2",
+			},
+		},
+		testHeader{
+			curlHeader: []string{"curl", "-X", "POST", "--header", "h1:v1", "--header", "h2:v2"},
+			need: H{
+				"h1": "v1",
+				"h2": "v2",
+			},
+		},
+	} {
+
+		code := 0
+		ts := createGeneralHeader(headerData.need, t)
+
+		req, err := ParseSlice(append(headerData.curlHeader, ts.URL)).Request()
+		assert.NoError(t, err)
+
+		gout.New().SetRequest(req).Debug(true).Code(&code).Do()
+		assert.Equal(t, code, 200)
+	}
 }
